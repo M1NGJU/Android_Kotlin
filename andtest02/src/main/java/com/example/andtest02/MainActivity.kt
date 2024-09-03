e147dec6d1476f5ac08830271325d300edecda8d
Minju <s2303@e-mirim.hs.kr>
2024-09-03 19:39:02 +0900

    뷰 예제

---
+++ andtest02/src/main/java/com/example/andtest02/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.andtest02
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        var btn3 = findViewById<Button>(R.id.btn3)
+        btn3.setOnClickListener{
+            btn3.visibility = View.VISIBLE
+        }
+    }
+}

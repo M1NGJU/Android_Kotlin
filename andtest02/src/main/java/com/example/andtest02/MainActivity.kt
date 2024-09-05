359f66c8930fd7baccc1b0b860ca49b4ee711ee9
Minju <s2303@e-mirim.hs.kr>
2024-09-05 19:52:04 +0900

    이미지관련 레이아웃

--- andtest02/src/main/java/com/example/andtest02/MainActivity.kt
+++ andtest02/src/main/java/com/example/andtest02/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.andtest02
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,10 +14,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        var btn3 = findViewById<Button>(R.id.btn3)
-        btn3.setOnClickListener{
-            btn3.visibility = View.VISIBLE
+        setContentView(R.layout.layout_test_3)
+        var edName = findViewById<EditText>(R.id.etName)
+        var btnOK = findViewById<Button>(R.id.btnOK)
+
+        btnOK.setOnClickListener{
+            var name = edName.text
+            Toast.makeText(applicationContext, "입력한 이름 :"+name, Toast.LENGTH_SHORT ).show()
         }
     }
 }
